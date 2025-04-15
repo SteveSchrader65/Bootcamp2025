77268b0306d84c3f070f6b493d678ad67dd79780
Steve S. <J294862@tafe.wa.edu.au>
2025-04-15 15:28:21 +0800

    products: Added code to put products in Collection.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import {connectDB} from './config/db.js'
+import Product from "./models/product.models.js"
 
 const app = express()
 
+app.use(express.json())
 dotenv.config()
 
 app.listen(5000, () => {
@@ -18,13 +20,13 @@ app.post("/api/products", async (req, res) => {
     return res.status(400).json({ success: false, message: 'Please provide all fields' })
   }
 
-  const newProduct = new product(product)
+  const newProduct = new Product(product)
 
   try {
     await newProduct.save()
     res.status(201).json({ success: true, data: newProduct })
   } catch (error) {
-    console.error("Error in product Creation: ", error)
+    console.error("Error in product Creation: ", error.message)
     res.status(500).json({ success: false, message: 'Server error' })
   }
 })
