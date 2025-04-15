7e5a93dfd36707a07fc1b171984bb33445e28507
Steve S. <J294862@tafe.wa.edu.au>
2025-04-15 15:09:56 +0800

    create Products function added

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,20 @@ app.listen(5000, () => {
   console.log("Hey Hey!! We're the Monkees!!")
 })
 
-app.get('/products', (req, res) => {
+app.post("/api/products", async (req, res) => {
+  const product = req.body
 
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({ success: false, message: 'Please provide all fields' })
+  }
+
+  const newProduct = new product(product)
+
+  try {
+    await newProduct.save()
+    res.status(201).json({ success: true, data: newProduct })
+  } catch (error) {
+    console.error("Error in product Creation: ", error)
+    res.status(500).json({ success: false, message: 'Server error' })
+  }
 })
