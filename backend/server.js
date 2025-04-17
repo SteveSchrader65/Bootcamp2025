9f50b1ff6289ab5a6bfa68691a219f05d06214b2
Steve S. <J294862@tafe.wa.edu.au>
2025-04-17 09:38:59 +0800

    feat:back-end files completed

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ import dotenv from 'dotenv'
 import {connectDB} from "./config/db.js"
 import productRoutes from "./routes/product.routes.js"
 
+const PORT = process.env.PORT || 5000
 const app = express()
 
 dotenv.config()
 app.use(express.json())
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB()
   console.log("Hey Hey!! We're the Monkees!!")
 })
